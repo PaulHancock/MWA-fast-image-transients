c03094128e3124061cc263a6a5527d0d365ad511
Paul Hancock <Paul.Hancock@curtin.edu.au>
2017-04-24 14:36:35 +0800

    create two image sets

--- bin/make_images.sh
+++ bin/make_images.sh
@@ -4,27 +4,30 @@
 #SBATCH --time=20:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=32gb
-#SBATCH --output=/scratch2/mwasci/phancock/G0026/queue/make_images.sh.o%A
-#SBATCH --error=/scratch2/mwasci/phancock/G0026/queue/make_images.sh.e%A
+#SBATCH --output=/scratch2/mwasci/phancock/D0009/queue/make_image.o%A
+#SBATCH --error=/scratch2/mwasci/phancock/D0009/queue/make_image.e%A
 
 aprun='aprun -n 1 -d 8 -b'
 
-# Get the new version of Aegean
-#export PATH=/group/mwaops/phancock/code/Aegean:$PATH
-#export PYTHONPATH=/group/mwaops/phancock/code/Aegean:$PYTHONPATH
-
-datadir=/scratch2/mwasci/phancock/G0026
+datadir=/scratch2/mwasci/phancock/D0009/processing
 
 cd $datadir
 
-obsnum=1102604896
+obsnum=OBSNUM
 ncpus=8
 
-$aprun wsclean -name ALOUETTE_noflagging/${obsnum}-sm -size 320 320 \
+# make the high time resolution images but don't clean
+$aprun wsclean -name ${obsnum}/${obsnum}-hr -size 320 320 \
                -weight briggs 0.5 -mfsweighting -scale 25.0amin \
                -pol I -niter 0 \
                -interval 0 232 -intervalsout 232 \
-               -channelrange 0 768 -channelsout 768 \
                -maxuv-l 32 -minuv-l 0.03 -smallinversion -j ${ncpus} \
-               satellite/${obsnum}/${obsnum}_norfi.ms
+               ${obsnum}/${obsnum}.ms
                #-joinchannels -stopnegative -joinpolarizations -niter 4000 -threshold 1.0 \ # don't clean
+
+# make the 2min image and clean
+$aprun wsclean -name ${obsnum}/${obsnum}-lr \
+               -weight briggs 0.5 -mfsweighting -scale 25.0amin \
+               -pol I -j ${ncpus} \
+               -joinchannels -stopnegative -joinpolarizations -niter 4000 -threshold 1.0 \
+               ${obsnum}/${obsnum}.ms
