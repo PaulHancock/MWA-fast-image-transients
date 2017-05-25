bca86c7dc6ada652c346dd743b2e1cc20c9d1d15
PaulHancock <paul.hancock@curtin.edu.au>
2017-05-25 16:17:59 +0800

    add link between grb and observation tables

--- db/import_observations_from_db.py
+++ db/import_observations_from_db.py
@@ -10,58 +10,79 @@ __author__ = "PaulHancock"
 BASEURL = 'http://mwa-metadata01.pawsey.org.au/metadata/'
 dbfile = 'MWA-GRB.sqlite'
 
+
 # Function to call a JSON web service and return a dictionary: This function by Andrew Williams
 def getmeta(service='obs', params=None):
-  """
-  Given a JSON web service ('obs', find, or 'con') and a set of parameters as
-  a Python dictionary, return a Python dictionary containing the result.
-  """
-  if params:
-    data = urllib.urlencode(params)  # Turn the dictionary into a string with encoded 'name=value' pairs
-  else:
-    data = ''
-  # Validate the service name
-  if service.strip().lower() in ['obs', 'find', 'con']:
-    service = service.strip().lower()
-  else:
-    print "invalid service name: %s" % service
+    """
+    Given a JSON web service ('obs', find, or 'con') and a set of parameters as
+    a Python dictionary, return a Python dictionary containing the result.
+    """
+    if params:
+        data = urllib.urlencode(params)  # Turn the dictionary into a string with encoded 'name=value' pairs
+    else:
+        data = ''
+    # Validate the service name
+    if service.strip().lower() in ['obs', 'find', 'con']:
+        service = service.strip().lower()
+    else:
+        print "invalid service name: %s" % service
+        return
+    # Get the data
+    try:
+        print BASEURL + service + '?' + data
+        result = json.load(urllib2.urlopen(BASEURL + service + '?' + data))
+    except urllib2.HTTPError as error:
+        print "HTTP error from server: code=%d, response:\n %s" % (error.code, error.read())
+        return
+    except urllib2.URLError as error:
+        print "URL or network error: %s" % error.reason
+        return
+    # Return the result dictionary
+    return result
+
+
+def update_observation(obsid, obsname, cur):
+    cur.execute("SELECT count(name) FROM grb WHERE fermi_trigger_id = ?", (obsname,))
+    if cur.fetchone()[0] > 0:
+        cur.execute("SELECT name FROM grb WHERE fermi_trigger_id = ?", (obsname,))
+        grb = cur.fetchone()[0]
+        cur.execute("UPDATE observation SET grb = ? WHERE obs_id =?", (grb, obsid))
     return
-  # Get the data
-  try:
-    print BASEURL + service + '?' + data
-    result = json.load(urllib2.urlopen(BASEURL + service + '?' + data))
-  except urllib2.HTTPError as error:
-    print "HTTP error from server: code=%d, response:\n %s" % (error.code, error.read())
-    return
-  except urllib2.URLError as error:
-    print "URL or network error: %s" % error.reason
-    return
-  # Return the result dictionary
-  return result
 
 
 def copy_obs_info(obsid, cur):
     cur.execute("SELECT count(*) FROM observation WHERE obs_id =?",(obsid,))
-    if cur.fetchone()[0] >0:
-        print "already imported", obsid
+    if cur.fetchone()[0] > 0:
+        #print "already imported", obsid
         return
     meta = getmeta(service='obs', params={'obs_id':obsid})
     metadata = meta['metadata']
     cur.execute("""
-INSERT OR REPLACE INTO observation
-(obs_id, projectid,  lst_deg, starttime, duration_sec, obsname, creator,
-azimuth_pointing, elevation_pointing, ra_pointing, dec_pointing,
-freq_res, int_time, grb, 
-calibration, cal_obs_id, calibrators, nfiles,
-archived
-)
-VALUES (?,?,?,?,?, ?,?,?,?,?, ?,?,?,?,?, ?,?,?,?);
-""", (
-obsid, meta['projectid'], metadata['local_sidereal_time_deg'], meta['starttime'], meta['stoptime']-meta['starttime'], meta['obsname'], meta['creator'],
-metadata['azimuth_pointing'], metadata['elevation_pointing'], metadata['ra_pointing'], metadata['dec_pointing'],
-meta['freq_res'], meta['int_time'], None,
-metadata['calibration'], None, metadata['calibrators'],len(meta['files']), False))
-    
+    INSERT OR REPLACE INTO observation
+    (obs_id, projectid,  lst_deg, starttime, duration_sec, obsname, creator,
+    azimuth_pointing, elevation_pointing, ra_pointing, dec_pointing,
+    freq_res, int_time, grb,
+    calibration, cal_obs_id, calibrators, nfiles,
+    archived
+    )
+    VALUES (?,?,?,?,?, ?,?,?,?,?, ?,?,?,?,?, ?,?,?,?);
+    """, (
+    obsid, meta['projectid'], metadata['local_sidereal_time_deg'], meta['starttime'], meta['stoptime']-meta['starttime'], meta['obsname'], meta['creator'],
+    metadata['azimuth_pointing'], metadata['elevation_pointing'], metadata['ra_pointing'], metadata['dec_pointing'],
+    meta['freq_res'], meta['int_time'], None,
+    metadata['calibration'], None, metadata['calibrators'], len(meta['files']), False))
+    update_observation(obsid, meta['obsname'], cur)
+    return
+
+
+def update_grb_links(cur):
+    cur.execute("SELECT obs_id, obsname FROM observation WHERE grb IS NULL")
+    # need to do this to exhaust the generator so we can reuse the cursor
+    obsids, obsnames = zip(*cur.fetchall())
+    for obsid, obsname in zip(obsids, obsnames):
+        update_observation(obsid, obsname, cur)
+    return
+
 
 if __name__ == "__main__":
     conn = sqlite3.connect(dbfile)
@@ -72,11 +93,7 @@ if __name__ == "__main__":
         obs_id = obs[0]
         copy_obs_info(obs_id, cur)
         conn.commit()
+
+    update_grb_links(cur)
+    conn.commit()
     conn.close()
-#obsinfo1 = getmeta(service='obs', params={'obs_id':starttime})
-#obsinfo2 = getmeta(service='obs', params={'filename':fname})
-
-#coninfo1 = getmeta(service='con', params={'obs_id':starttime})
-#coninfo2 = getmeta(service='con', params={'filename':fname})
-
-#olist = getmeta(service='find', params={'mintime':1097836168, 'maxtime':1097840480, 'obsname':'3C444%'})
