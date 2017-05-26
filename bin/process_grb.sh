f7f616068bc4d39ba683eb2116dc0bdbce12dab7
Paul Hancock <Paul.Hancock@curtin.edu.au>
2017-05-26 16:02:52 +0800

    fix dependency checking and bash syntax

--- bin/process_grb.sh
+++ bin/process_grb.sh
@@ -4,7 +4,7 @@ grb=$1
 
 if [[ ! ${grb} ]]
 then
-    echo "get_cal.sh grbname"
+    echo "process_grb.sh grbname"
     exit 1
 fi
 
@@ -13,20 +13,29 @@ res=(`sqlite3 db/MWA-GRB.sqlite "SELECT obs_id, obsname FROM observation WHERE g
 calid=${res[0]}
 calname=${res[1]}
 
-solutions=`ls processing/${obsid}/${calid}*solutions.bin`
+solutions=`ls processing/${calid}/${calid}*solutions.bin 2>/dev/null`
 if [[ $? ]]
 then
     # the solutions don't exist so we must make them
     echo "Making calibration solutions"
-    ./bin/obs_dl.sh ${calid}
+    # setting calname means that we chain onto calculating the calibration solution
+    ./bin/obs_dl.sh ${calid} 0 0 ${calname}
     exit $?
 fi
 
 echo "Solutions exist, proceccing target observations"
 obsids=(`sqlite3 db/MWA-GRB.sqlite "SELECT obs_id FROM observation WHERE grb=\"${grb}\" AND NOT calibration"`)
+depend=0
 for id in obsids
 do
     # do all the processing for this obsid
-    ./bin/obs_dl.sh ${obsid} 0 ${calid} ${calname}
+    # supplying the calibration id means that we chain onto applying the cal solution and then imaging
+    dep=(`./bin/obs_dl.sh ${id} ${depend} ${calid} 0`)
+    if [[ $? ]]
+    then
+	depend=${dep[3]}
+    else
+	exit $?
+    fi
 done
     
