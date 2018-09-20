16f8f4ead4b7bd81269c88763411f0bdf690bd32
Paul Hancock <Paul.Hancock@curtin.edu.au>
2018-09-20 15:29:48 +0800

    update 0.5s and 28s scripts as per #27

--- bin/obs_im05s.sh
+++ bin/obs_im05s.sh
@@ -1,30 +1,85 @@
 #! /bin/bash
 
-obsnum=$1
-dep=$2
+usage()
+{
+echo "obs_im05s.sh [-d dep] [-q queue] [-s imsize] [-p pixscale] [-t] obsnum
+  -d dep     : job number for dependency (afterok)
+  -q queue   : job queue, default=gpuq
+  -s imsize  : image size will be imsize x imsize pixels, default 4096
+  -p pixscale: image pixel scale, default is 32asec
+  -t         : test. Don't submit job, just make the batch file
+               and then return the submission command
+  obsnum     : the obsid to process" 1>&2;
+exit 1;
+}
 
+#initialize as empty
+dep=
+queue='-p gpuq'
+imsize=
+pixscale=
+tst=
+
+# parse args and set options
+while getopts ':tcd:q:s:p:' OPTION
+do
+    case "$OPTION" in
+	d)
+	    dep=${OPTARG}
+	    ;;
+	q)
+	    queue="-p ${OPTARG}"
+	    ;;
+	s)
+	    imsize=${OPTARG}
+	    ;;
+	p)
+	    pixscale=${OPTARG}
+	    ;;
+	t)
+	    tst=1
+	    ;;
+	? | : | h)
+	    usage
+	    ;;
+  esac
+done
+# set the obsid to be the first non option
+shift  "$(($OPTIND -1))"
+obsnum=$1
+
+# if obsid is empty then just pring help
 if [[ -z ${obsnum} ]]
 then
-    echo "usage: obs_im05s.sh obs_id [dependency]"
-    exit 1
+    usage
 fi
 
-depend=""
-if [[ ! -z ${dep} ]] 
+
+if [[ ! -z ${dep} ]]
 then
-depend="--dependency=afterok:${dep}"
+    depend="--dependency=afterok:${dep}"
 fi
 
 base='/astro/mwasci/phancock/D0009/'
 
 script="${base}queue/im05s_${obsnum}.sh"
-cat ${base}/bin/im05s.tmpl | sed "s:OBSNUM:${obsnum}:g" | sed "s:BASEDIR:${base}:g"  > ${script}
+cat ${base}/bin/im05s.tmpl | sed -e "s:OBSNUM:${obsnum}:g" \
+                                 -e "s:BASEDIR:${base}:g"  > ${script}
 
 output="${base}queue/logs/im05s_${obsnum}.o%A"
 error="${base}queue/logs/im05s_${obsnum}.e%A"
 
+sub="sbatch --begin=now+15 --output=${output} --error=${error} ${depend} ${queue} ${script}"
+if [[ ! -z ${tst} ]]
+then
+    echo "script is ${script}"
+    echo "submit via:"
+    echo "${sub}"
+    exit 0
+fi
+
 # submit job
-jobid=(`sbatch --begin=now+15 --output=${output} --error=${error} ${depend} ${queue} ${script}`)
+jobid=($(${sub}))
 jobid=${jobid[3]}
 
 # rename the err/output files as we now know the jobid
