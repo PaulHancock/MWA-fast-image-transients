7a521c333a0d7cf34bb3352ba6cb667906660971
Paul Hancock <Paul.Hancock@curtin.edu.au>
2018-09-27 14:00:09 +0800

    update account infos

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,7 @@
 
 # SET UP YOUR VARIABLES HERE
 base='/astro/mwasci/phancock/RadioSchool/MWA-fast-image-transients/'
+account='mwasci'
 
-
-sed -i s:^base=.*:base=${base}:g obs_*.sh
+sed -i -e s:^base=.*:base=${base}:g obs_*.sh
+sed -i -e s:account=.*:account=${account}:g *.tmpl asvo_wget.sh
