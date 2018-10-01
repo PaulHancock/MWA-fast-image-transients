f01e429757dd76c6a65e8b4f393aac0cfa93c632
PaulHancock <paul.hancock@curtin.edu.au>
2018-10-01 11:15:18 +0800

    add quotes

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,4 +5,4 @@ base='/group/courses01/${USER}/mwa'
 account='courses01'
 reservation='courseq'
 sed -i -e s:^base=.*:base=${base}:g obs_*.sh
-sed -i -e s:account=.*:account=${account}\n#SBATCH --reservation=${reservation}:g *.tmpl asvo_wget.sh
+sed -i -e "s:account=.*:account=${account}\n#SBATCH --reservation=${reservation}:g" *.tmpl asvo_wget.sh
