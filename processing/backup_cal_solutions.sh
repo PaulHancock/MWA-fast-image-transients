33e9dd8bb8a0887d99031cfcf8291a29a2a934d0
Paul Hancock <Paul.Hancock@curtin.edu.au>
2019-01-09 10:41:06 +0800

    only backup directories that look like obsids

--- processing/backup_cal_solutions.sh
+++ processing/backup_cal_solutions.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-for d in $( ls -d * ); do mkdir -p ../done/calibration_solutions/${d}; cp ${d}/*.{bin,txt,png} ../done/calibration_solutions/${d}/; done;
+for d in $( ls -d [0-9]*[0-9] ); do mkdir -p ../done/calibration_solutions/${d}; cp ${d}/*.{bin,txt,png} ../done/calibration_solutions/${d}/; done;
