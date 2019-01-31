2f2a4000053428ca2619a762e00aa533231af612
Paul Hancock <Paul.Hancock@curtin.edu.au>
2019-01-31 16:09:15 +0800

    add get_mem and get_cores

--- bin/functions.sh
+++ bin/functions.sh
@@ -12,3 +12,44 @@ then
     exit $1
 fi
 }
+
+
+function get_mem {
+    if [[ ${SLURM_CLUSTER_NAME} == 'zeus' ]] ; then
+	# default
+	mem=64
+	if [[ ${SLURM_JOB_PARTITION} == 'workq' ]] ; then
+	    mem=128
+	fi
+    elif [[ ${SLURM_CLUSTER_NAME} == 'galaxy' ]] ; then
+	# gpuq
+	mem=32
+    elif [[ ${SLURM_CLUSTER_NAME} == 'magnus' ]] ; then
+	# workq
+	mem=64
+    else
+	echo "Unknown cluster, unable to determine memory assuming 32" >&2
+	mem=32
+    fi
+    echo ${mem}
+}
+
+function get_cores {
+    if [[ ${SLURM_CLUSTER_NAME} == 'zeus' ]] ; then
+	# default
+	cores=1
+	if [[ ${SLURM_JOB_PARTITION} == 'workq' ]] ; then
+	    cores=28
+	fi
+    elif [[ ${SLURM_CLUSTER_NAME} == 'galaxy' ]] ; then
+	# gpuq
+	cores=16
+    elif [[ ${SLURM_CLUSTER_NAME} == 'magnus' ]] ; then
+	# workq
+	cores=48
+    else
+	echo "Unknown cluster, unable to determine cores assuming 16" >&2
+	cores=16
+    fi
+    echo ${cores}
+}
