083ae550f2e3069c710e27fb0263f25ee4cbdf5a
Paul Hancock <Paul.Hancock@curtin.edu.au>
2020-10-28 10:04:36 +0800

    python2/3 compatability changes

--- bin/fitssub.py
+++ bin/fitssub.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from astropy.io import fits as pyfits
 
 import sys,os
@@ -15,18 +16,18 @@ def get_time_from_header(hdu):
 
 
 file1,file2,out=sys.argv[1:4]
-print "combining {0}-{1}={2}".format(file1,file2,out)
+print("combining {0}-{1}={2}".format(file1,file2,out))
 
 if not os.path.exists(file1):
-    print "can't find {0}".format(file1)
+    print("can't find {0}".format(file1))
 if not os.path.exists(file2):
-    print "can't find {0}".format(file2)
+    print("can't find {0}".format(file2))
 
 fits1 = pyfits.open(file1)
 fits2 = pyfits.open(file2)
 
 if fits1[0].data.shape != fits2[0].data.shape:
-    print "incompatible file sizes {0} and {1}".format(fits1[0].data.shape,fits2[0].data.shape)
+    print("incompatible file sizes {0} and {1}".format(fits1[0].data.shape,fits2[0].data.shape))
     sys.exit(1)
 
 fits1[0].data-=fits2[0].data
@@ -42,4 +43,3 @@ fits1[0].header['DATE-OBS'] = mid.strftime(format='%Y-%m-%dT%H:%M:%S.%f')
 fits1[0].header['HISTORY'] = "Diff image"
 fits1[0].header['HISTORY'] = "{0} - {1} = this file".format(file1, file2)
 fits1.writeto(out,overwrite=True)
-#fits1.writeto(out,clobber=True)
