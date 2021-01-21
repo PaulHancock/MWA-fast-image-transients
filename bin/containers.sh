9ef7e64bb79c1f45bc698ad65993d342dd37188c
Paul Hancock <Paul.Hancock@curtin.edu.au>
2021-01-21 12:05:27 +0800

    always use containers on all systems

--- bin/containers.sh
+++ bin/containers.sh
@@ -7,19 +7,16 @@ Cwsclean=''
 Crfiseeker=''
 Cgleamx=''
 
-# If we are on garrawarla then set up the containers
-if [[ ${SLURM_CLUSTER_NAME} == 'garrawarla' ]] ; then
-  container_base="/pawsey/mwa/singularity"
-  container_exec="singularity exec -B $PWD"
+container_base="/pawsey/mwa/singularity"
+container_exec="singularity exec -B $PWD"
   
-  # need to super-hack the path to make sure that the .h5 beam file is found within the right python path within the container
-  Creduce="${container_exec} -B /pawsey/mwa:/usr/lib/python3/dist-packages/mwapy/data ${container_base}/mwa-reduce/mwa-reduce.img"
-  # these work nice as is
-  Ccotter="${container_exec} ${container_base}/cotter/cotter_latest.sif"
-  Cmwalib="${container_exec} ${container_base}/pymwalib/pymwalib_latest.sif"
-  Crobbie="${container_exec} ${container_base}/robbie/robbie-next.sif"
-  Cwsclean="${container_exec} ${container_base}/wsclean/wsclean_2.9.2.img"
-  Crfiseeker="${container_exec} ${container_base}/rfi_seeker/rfi_seeker.img"
-  Cgleamx="${container_exec} -B /astro/mwasci/tgalvin/GLEAM_Pipeline/GLEAM-X-pipeline/data/pb:/pb_lookup/ /astro/mwasci/tgalvin/gleamx_testing_small.img"
-fi
+# need to super-hack the path to make sure that the .h5 beam file is found within the right python path within the container
+Creduce="${container_exec} -B /pawsey/mwa:/usr/lib/python3/dist-packages/mwapy/data ${container_base}/mwa-reduce/mwa-reduce.img"
+# these work nice as is
+Ccotter="${container_exec} ${container_base}/cotter/cotter_latest.sif"
+Cmwalib="${container_exec} ${container_base}/pymwalib/pymwalib_latest.sif"
+Crobbie="${container_exec} ${container_base}/robbie/robbie-next.sif"
+Cwsclean="${container_exec} ${container_base}/wsclean/wsclean_2.9.2.img"
+Crfiseeker="${container_exec} ${container_base}/rfi_seeker/rfi_seeker.img"
+Cgleamx="${container_exec} -B /astro/mwasci/tgalvin/GLEAM_Pipeline/GLEAM-X-pipeline/data/pb:/pb_lookup/ /astro/mwasci/tgalvin/gleamx_testing_small.img"
 
